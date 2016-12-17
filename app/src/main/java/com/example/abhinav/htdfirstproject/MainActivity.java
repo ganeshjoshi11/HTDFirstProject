ebab149eab2f0bfe42065ad67615d99cabe5a396
Ganesh Pd. Joshi <abhinavjoshi11@gmail.com>
2016-12-17 21:02:16 +0545

    This is the Initial Layout

---
+++ app/src/main/java/com/example/abhinav/htdfirstproject/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.abhinav.htdfirstproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Button button = (Button) findViewById(R.id.btnOk);
+        button.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        Log.i(TAG, "onClick: username");
+
+
+            }
+        });
+
+
+    }
+}
